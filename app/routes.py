e23cf5143ae3a1cd493f84106436613c7c32aa58
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-28 15:51:53 +0000

    Added login form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Index', user=user, entries=entries)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Log In', form=form)
