1d7e64ed13f8f998a7fc867c0fc1b8eeb2717f15
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-28 16:49:51 +0000

    Added login with temp flash

--- app/routes.py
+++ app/routes.py
@@ -18,7 +18,11 @@ def index():
     ]
     return render_template('index.html', title='Index', user=user, entries=entries)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on.submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+            return redirect('/index')
     return render_template('login.html', title='Log In', form=form)
