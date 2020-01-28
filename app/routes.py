8a7928ab6ce98e96f627beaedd9b8ef90f6a584f
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-28 15:20:48 +0000

    Updated Routes

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'DCurrey'}
-    return '''
-<html>
-    <head>
-        <title>Index - LearningFlask</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''.</h1>
-    </body>
-</html>'''
+    return render_template('index.html', title='Index', user=user)
