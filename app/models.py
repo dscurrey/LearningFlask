88788c6dcd6f949e2670b7db3ff19f5da1a02823
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-28 18:27:21 +0000

    Added functionality to model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
+from app import login
 from werkzeug.security import check_password_hash
 from datetime import datetime
 from app import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -25,3 +27,7 @@ class Entry(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
