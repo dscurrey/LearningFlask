7fe16a4e643efbae20f83057395e85356d8785e0
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-28 15:27:14 +0000

    Added for to html

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'DCurrey'}
-    return render_template('index.html', title='Index', user=user)
+    entries = [
+        {
+            'author': {'username': 'Dave'},
+            'body': 'Dave\'s Body Text'
+        },
+        {
+            'author': {'username': 'NotDave'},
+            'body': 'Some text also'
+        }
+    ]
+    return render_template('index.html', title='Index', user=user, entries=entries)
