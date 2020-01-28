e22255532aedf46a9548511f98ec70dd72d8a4d9
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-01-28 21:52:44 +0000

    Added error emailing support

--- app/__init__.py
+++ app/__init__.py
@@ -12,3 +12,19 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='FLASK ERROR',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
